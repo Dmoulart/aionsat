b88ccd93bf1f0345fce9bb7f0a3a53e9ca390888
dmoulart <dorian@resalocal.com>
2022-03-05 11:10:36 +0100

    Modify sandboxé

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -11,72 +11,64 @@ document.body.appendChild(canvas);
 const ctx = canvas.getContext('2d');
 ctx.strokeStyle = 'white';
 
-const circleA = new Circle(new Vector(0, 0), 10);
-const circleB = new Circle(new Vector(0, 0), 20);
+// Instantiate user polygon
+const polyA = new Polygon(vec(window.innerWidth / 2, window.innerHeight / 2), [vec(0, 0), vec(100, 0), vec(100, 100)]);
+
+// Instantiate other shapes
+const square = new Polygon(vec(200, 200), [
+  vec(0, 0),
+  vec(100, 0),
+  //vec(10, 10),
+  vec(0, 100)
+]);
+const squareB = new Polygon(vec(200, 200), [vec(0, 0), vec(10, 0), vec(10, 10), vec(0, 10)]);
+document.onmousemove = (e: MouseEvent) => {
+  // circleA.pos = vec(e.clientX, e.clientY)
+  squareB.pos = vec(e.clientX, e.clientY);
+};
+
+const circleA = new Circle(vec(200, 200), 10);
+document.onmousemove = (e: MouseEvent) => {
+  square.pos = vec(e.clientX, e.clientY);
+};
+const circleB = new Circle(vec(400, 200), 120);
+
 const sat = new Sat();
-console.log(sat.intersects(circleA, circleB));
 
-// // Instantiate user polygon
-// const polyA = new Polygon(vec(window.innerWidth / 2, window.innerHeight / 2), [vec(0, 0), vec(100, 0), vec(100, 100)]);
+(function loop() {
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
 
-// // Instantiate other shapes
-// const square = new Polygon(vec(200, 200), [
-//   vec(0, 0),
-//   vec(100, 0),
-//   //vec(10, 10),
-//   vec(0, 100)
-// ]);
-// const squareB = new Polygon(vec(200, 200), [vec(0, 0), vec(10, 0), vec(10, 10), vec(0, 10)]);
-// document.onmousemove = (e: MouseEvent) => {
-//   // circleA.pos = vec(e.clientX, e.clientY)
-//   squareB.pos = vec(e.clientX, e.clientY);
-// };
+  const collision = sat.intersects(square, circleA);
 
-// const circleA = new Circle(vec(200, 200), 10);
-// document.onmousemove = (e: MouseEvent) => {
-//   square.pos = vec(e.clientX, e.clientY);
-// };
-// const circleB = new Circle(vec(400, 200), 120);
+  drawPolygon(square);
 
-// const sat = new Sat();
+  drawCircle(circleA, collision ? 'red' : 'white');
 
-// (function loop() {
-//   ctx.clearRect(0, 0, innerWidth, innerHeight);
+  requestAnimationFrame(loop);
+})();
 
-//   const collision = sat.intersects(square, circleA);
+function resolve(collision: { overlap: number; normal: Vector }, shape: Shape) {
+  shape.pos = shape.pos.add(collision.normal.scale(collision.overlap + 1));
+}
 
-//   drawPolygon(square);
-//   //drawPolygon(squareB, collision && collision.bInA ? "red" : "white")
+function drawPolygon(polygon: Polygon, color = 'white') {
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  polygon.vertices.forEach((v, i) => {
+    if (i === 0) {
+      ctx.moveTo(v.x, v.y);
+    } else {
+      ctx.lineTo(v.x, v.y);
+    }
+  });
+  ctx.closePath();
+  ctx.stroke();
+}
 
-//   // console.log(collision)
-//   drawCircle(circleA, collision && collision.bInA ? 'red' : 'white');
-//   // drawCircle(circleB, collision && collision.aInB ? "red" : "white")
-
-//   requestAnimationFrame(loop);
-// })();
-
-// function resolve(collision: { overlap: number; normal: Vector }, shape: Shape) {
-//   shape.pos = shape.pos.add(collision.normal.scale(collision.overlap + 1));
-// }
-
-// function drawPolygon(polygon: Polygon, color = 'white') {
-//   ctx.strokeStyle = color;
-//   ctx.beginPath();
-//   polygon.vertices.forEach((v, i) => {
-//     if (i === 0) {
-//       ctx.moveTo(v.x, v.y);
-//     } else {
-//       ctx.lineTo(v.x, v.y);
-//     }
-//   });
-//   ctx.closePath();
-//   ctx.stroke();
-// }
-
-// function drawCircle(circle: Circle, color = 'white') {
-//   ctx.strokeStyle = color;
-//   ctx.beginPath();
-//   ctx.arc(circle.pos.x, circle.pos.y, circle.radius, 0, Math.PI * 2);
-//   ctx.stroke();
-//   ctx.closePath();
-// }
+function drawCircle(circle: Circle, color = 'white') {
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.arc(circle.pos.x, circle.pos.y, circle.radius, 0, Math.PI * 2);
+  ctx.stroke();
+  ctx.closePath();
+}
